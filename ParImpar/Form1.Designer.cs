40b6919ce4288e4a1d460054167d7bd616e4ed81
NickX zXn <gamerplayernick714@hotmail.com>
2021-11-30 17:23:56 -0500

    Agregar archivos de proyecto.

---
+++ ParImpar/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace ParImpar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnumero = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnUbica = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.errorProve = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProve)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ingresar un numero y colocar en el combobox \r\nsi es par y en el listbox si es imp" +
+    "ar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero";
+            // 
+            // txtnumero
+            // 
+            this.txtnumero.Location = new System.Drawing.Point(200, 80);
+            this.txtnumero.Name = "txtnumero";
+            this.txtnumero.Size = new System.Drawing.Size(114, 23);
+            this.txtnumero.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Pares";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(184, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Impares";
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(36, 171);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(91, 23);
+            this.comboBox.TabIndex = 5;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 15;
+            this.listBox.Location = new System.Drawing.Point(176, 169);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(175, 169);
+            this.listBox.TabIndex = 6;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(412, 107);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(114, 40);
+            this.btnNew.TabIndex = 7;
+            this.btnNew.Text = "Nuevo";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnUbica
+            // 
+            this.btnUbica.Location = new System.Drawing.Point(412, 46);
+            this.btnUbica.Name = "btnUbica";
+            this.btnUbica.Size = new System.Drawing.Size(114, 40);
+            this.btnUbica.TabIndex = 8;
+            this.btnUbica.Text = "Ubicar";
+            this.btnUbica.UseVisualStyleBackColor = true;
+            this.btnUbica.Click += new System.EventHandler(this.btnUbica_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(412, 171);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(114, 40);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Cerrar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // errorProve
+            // 
+            this.errorProve.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 389);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnUbica);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtnumero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Ubicador de Pares e Impares";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProve)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnumero;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnUbica;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ErrorProvider errorProve;
+    }
+}
+
---
+++ ParImpar/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParImpar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtnumero.Clear();
+            comboBox.Items.Clear();
+            listBox.Items.Clear();
+            txtnumero.Focus();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Desea Salir?", "Alerta", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnUbica_Click(object sender, EventArgs e)
+        {
+            if(txtnumero.Text=="")
+            {
+                errorProve.SetError(txtnumero, "Agrege un Numero");
+                txtnumero.Focus();
+                return;
+            }
+            errorProve.SetError(txtnumero, "");
+            int value = int.Parse(txtnumero.Text);
+            if(value%2==0)
+            {
+                comboBox.Items.Add(value);
+            }
+            else
+            {
+                listBox.Items.Add(value);
+            }
+        }
+    }
+}
